30c7891398dfe770fd3a3079669c2dea274a7e26
Camila Dias dos Santos <camiladiasds@gmail.com>
2021-07-16 18:26:08 -0300

    Simple example about encapsulation in the class ContaBancaria

---
+++ Encapsulation/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Encapsulation
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string nome)
+        {
+            Numero = numero;
+            Titular = nome;
+        }
+
+        public ContaBancaria(int numero, string nome, double saldo) :this (numero, nome)
+        {
+            Deposito(saldo);
+        }
+
+        public void Deposito(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            Saldo -= valorSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Numero + ", Titular: " + Titular + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string nome;
+            char resposta;
+            double depositoInicial, deposito, saque;
+
+            ContaBancaria conta;
+            
+
+            Console.Write("Entre o número da conta:");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta:");
+            nome = Console.ReadLine();
+
+            Console.Write("Haverá saldo inicial? (s/n)");
+            resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's' || resposta == 'S' )
+            {
+                Console.Write("Entre o valor do depósito inicial:");
+                depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, nome, depositoInicial);
+            }
+            else
+                conta = new ContaBancaria(numero, nome);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre o valor do depósito:");
+            deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre o valor do saque:");
+            saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta atualizados:");
+            Console.Write(conta);
+        }
+    }
+}
